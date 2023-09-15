d19d7edae758163e63127f88a119c5efbeadb546
Sloutmyv <clercsyl9123@gmail.com>
2023-09-15 16:24:58 +1100

    food_trade

--- trade_food.py
+++ trade_food.py
@@ -40,44 +40,44 @@ df_net = df_net.rename(columns={'IMPORTER: Importer': 'IMPORTATEURS',
 
 # print(df_net.head())
 
-#########################
-##### ANALYSE #####
-#########################
-# df_TF_1 = df_net.groupby('IMPORTATEURS').agg(
-#     {'VALEUR_T': 'sum'}).sort_values('VALEUR_T', ascending=False)
+########################
+#### ANALYSE #####
+########################
+df_TF_1 = df_net.groupby('IMPORTATEURS').agg(
+    {'VALEUR_T': 'sum'}).sort_values('VALEUR_T', ascending=False)
 
-# total_import_ton = df_net['VALEUR_T'].sum()
+total_import_ton = df_net['VALEUR_T'].sum()
 
-# df_TF_1['PCT_VALEUR_T'] = round(df_TF_1['VALEUR_T']*100/total_import_ton, 2)
-# df_TF_1['PCT_VALEUR_T_CUMSUM'] = round(df_TF_1['PCT_VALEUR_T'].cumsum(), 2)
+df_TF_1['PCT_VALEUR_T'] = round(df_TF_1['VALEUR_T']*100/total_import_ton, 2)
+df_TF_1['PCT_VALEUR_T_CUMSUM'] = round(df_TF_1['PCT_VALEUR_T'].cumsum(), 2)
 
-# print(total_import_ton)
+print(total_import_ton)
 
-# # Graphique GTF_1
-# graph_title = "FOOD TRADE - Répartition quantité importée totale par pays de 1995 à 2018"
-# title_family = 'serif'  # 'serif' | 'sans-serif' | 'cursive' | 'fantasy' | 'monospace'
-# title_color = 'midnightblue'
-# # 'normal' | 'bold' | 'heavy' | 'light' | 'ultrabold' | 'ultralight'
-# title_weight = 'bold'
-# title_size = 14
-# title_x = 0.5
-# title_y = 1
+# Graphique GTF_1
+graph_title = "FOOD TRADE - Répartition quantité importée totale par pays de 1995 à 2018"
+title_family = 'serif'  # 'serif' | 'sans-serif' | 'cursive' | 'fantasy' | 'monospace'
+title_color = 'midnightblue'
+# 'normal' | 'bold' | 'heavy' | 'light' | 'ultrabold' | 'ultralight'
+title_weight = 'bold'
+title_size = 14
+title_x = 0.5
+title_y = 1
 
-# fig, axe1 = plt.subplots(nrows=1, ncols=1, figsize=(20, 10), sharex=True)
-# axe1.set_title(graph_title, x=title_x, y=title_y, fontdict={
-#                'family': title_family, 'color': title_color, 'weight': title_weight, 'size': title_size})
-
-
-# def autopct_tunning(p): return '{:1.1f}%'.format(p) if p > 1 else None
+fig, axe1 = plt.subplots(nrows=1, ncols=1, figsize=(20, 10), sharex=True)
+axe1.set_title(graph_title, x=title_x, y=title_y, fontdict={
+               'family': title_family, 'color': title_color, 'weight': title_weight, 'size': title_size})
 
 
-# axe1.pie(df_TF_1['PCT_VALEUR_T'],
-#          labels=[x if df_TF_1.loc[df_TF_1.index.values == x, 'PCT_VALEUR_T'].iloc[0] > 1 else '' for x in df_TF_1.index.values], autopct=autopct_tunning, textprops={'fontsize': 8},)
+def autopct_tunning(p): return '{:1.1f}%'.format(p) if p > 1 else None
 
-# # axe1.legend(loc="center", bbox_to_anchor=(0, 0, 0, 0),
-# #             ncol=2, fancybox=True, shadow=True)
 
-# plt.show()
+axe1.pie(df_TF_1['PCT_VALEUR_T'],
+         labels=[x if df_TF_1.loc[df_TF_1.index.values == x, 'PCT_VALEUR_T'].iloc[0] > 1 else '' for x in df_TF_1.index.values], autopct=autopct_tunning, textprops={'fontsize': 8},)
+
+# axe1.legend(loc="center", bbox_to_anchor=(0, 0, 0, 0),
+#             ncol=2, fancybox=True, shadow=True)
+
+plt.show()
 ############################################################
 # df_TF_2 = df_net.groupby('PRODUITS').agg(
 #     {'VALEUR_T': 'sum'}).sort_values('VALEUR_T', ascending=False)
@@ -111,19 +111,19 @@ df_net = df_net.rename(columns={'IMPORTER: Importer': 'IMPORTATEURS',
 # plt.show()
 ############################################################
 
-df_TF_3 = df_net.groupby(['PRODUITS', 'IMPORTATEURS'],
-                         as_index=False).agg({'VALEUR_T': 'sum'})
-df_TF_3_pivot = df_TF_3.pivot_table(
-    columns='IMPORTATEURS', index='PRODUITS', values='VALEUR_T')
-df_TF_3_pivot.loc[:, 'Row_Total'] = df_TF_3_pivot.sum(
-    numeric_only=True, axis=1)
-df_TF_3_pivot = df_TF_3_pivot.sort_values(by=['Row_Total'], ascending=False)
-df_TF_3_pivot = df_TF_3_pivot[df_TF_3_pivot.sum(
-).sort_values(ascending=False).index]
-df_TF_3_pivot = df_TF_3_pivot.iloc[:, :].apply(lambda x: x / x.sum())
+# df_TF_3 = df_net.groupby(['PRODUITS', 'IMPORTATEURS'],
+#                          as_index=False).agg({'VALEUR_T': 'sum'})
+# df_TF_3_pivot = df_TF_3.pivot_table(
+#     columns='IMPORTATEURS', index='PRODUITS', values='VALEUR_T')
+# df_TF_3_pivot.loc[:, 'Row_Total'] = df_TF_3_pivot.sum(
+#     numeric_only=True, axis=1)
+# df_TF_3_pivot = df_TF_3_pivot.sort_values(by=['Row_Total'], ascending=False)
+# df_TF_3_pivot = df_TF_3_pivot[df_TF_3_pivot.sum(
+# ).sort_values(ascending=False).index]
+# df_TF_3_pivot = df_TF_3_pivot.iloc[:, :].apply(lambda x: x / x.sum())
 
-print(df_TF_3_pivot)
+# print(df_TF_3_pivot)
 
 
-sns.heatmap(df_TF_3_pivot, square=True, norm=LogNorm())
-plt.show()
+# sns.heatmap(df_TF_3_pivot, square=True, norm=LogNorm())
+# plt.show()
